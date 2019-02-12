312f6c81ea67d46c1abe58279b720fe8af4a285c
Matthew Lee <yleematthew@gmail.com>
2019-02-12 03:34:39 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,14 +1,14 @@
 def my_select(collection)
  # your code here!
- if block_given?
+if block_given?
     i = 0
     selected = []
     while i < collection.length
-      selected collection[i] if yield(collection[i]) == true
+      selected.push(collection[i]) if yield(collection[i]) == true
       i += 1
     end
   else
     puts "This block should not run!"
   end
   selected
-end
+end	end
