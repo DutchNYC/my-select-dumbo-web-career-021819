4a60e938867e87cd67bb758250d798a47dbfa5e7
Matthew Lee <yleematthew@gmail.com>
2019-02-12 03:30:16 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
     i = 0
     selected = []
     while i < collection.length
-      selected(collection[i]) if yield(collection[i]) == true
+      selected collection[i] if yield(collection[i]) == true
       i += 1
     end
   else
