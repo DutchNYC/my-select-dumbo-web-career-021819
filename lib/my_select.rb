09f334d5b114adee7ac131b434f4e5973db27e74
Matthew Lee <yleematthew@gmail.com>
2019-02-12 03:09:35 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
  # your code here!
-end
+ if block_given?
+    i = 0
+    selected = []
+    while i < collection.length
+      selected.push(collection[i]) if yield(collection[i]) == true
+      i += 1
+    end
+  else
+    puts "No block given"
+  end
+  selected
+end	end
